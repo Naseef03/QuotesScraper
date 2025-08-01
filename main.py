431f1ba5be57d4b23316267767f7168d1a5cc748
Naseef03 <muhammednaseef03@gmail.com>
2025-08-01 22:54:08 +0530

    Adds multiple page scraping feature to main.py
    Changes ommitted:
    	modified:   data/quotes.csv
    	modified:   main.py

--- main.py
+++ main.py
@@ -4,22 +4,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 # Constants
-page_link = "https://quotes.toscrape.com"
-quotes_limit = 100
 save_file_path = "data/quotes.csv"
 
 if __name__ == "__main__":
-    # Get the page
-    response = requests.get(page_link)
-
-    # Check the status
-    if response.status_code != 200:
-        print("Can't access site")
-        sys.exit()
-    
-    # Parse HTML
-    soup = BeautifulSoup(response.text, "html.parser")
-
     # DataFrame Input Object
     quote_data = {
         "Text": [],
@@ -27,26 +14,46 @@ if __name__ == "__main__":
         "Tags": [],
     }
 
-    # Find Quotes
-    quotes = soup.find_all("div", class_="quote", limit=100)
+    print("Started Scraping Quotes")
 
-    for quote in quotes: 
-        # Find Quote text
-        quote_data["Text"].append(quote.find("span", class_="text").text)
+    # Scrape Quotes
+    for i in range(10):
+        page_link = "https://quotes.toscrape.com/page/" + str(i + 1)
+        
+        # Get the page
+        response = requests.get(page_link)
 
-        # Find Quote Author
-        quote_data["Author"].append(quote.find("small", class_="author").text)
+        # Check the status
+        if response.status_code != 200:
+            print("Can't access site")
+            break
+        
+        # Parse HTML
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        # Find Quote Tags
-        tag_list = []
-        tags = quote.find_all("a", class_="tag")
-        for tag in tags:
-            tag_list.append(tag.text)
-        tag_str = ", ".join(tag_list)
-        quote_data["Tags"].append(tag_str)
+        # Find Quotes
+        quotes = soup.find_all("div", class_="quote")
+
+        for quote in quotes: 
+            # Find Quote text
+            quote_data["Text"].append(quote.find("span", class_="text").text)
+
+            # Find Quote Author
+            quote_data["Author"].append(quote.find("small", class_="author").text)
+
+            # Find Quote Tags
+            tag_list = []
+            tags = quote.find_all("a", class_="tag")
+            for tag in tags:
+                tag_list.append(tag.text)
+            tag_str = ", ".join(tag_list)
+            quote_data["Tags"].append(tag_str)
+        
+        print(f"Scraped page {i + 1}")
+
+    print("Finished Scraping Quotes")
 
     # Create DataFrame
     df = pd.DataFrame(quote_data)
-    print(df)
     df.to_csv(save_file_path, index=False)
        
